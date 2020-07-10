362b8eec4ef7566a6617dd97d7705d4c1273b773
Robert Koziarski <rob.koziarski2k@gmail.com>
2020-07-10 13:47:37 +0200

    do poprawe plansza - w ciągu dalszym, dodano przycisk w intro

--- source/interfejsgry.py
+++ source/interfejsgry.py
@@ -25,12 +25,12 @@ win = pygame.image.load(buttons.sciezka_do_img('back_tic_tac_toe.png'))
 
 
 # grafika planszy
-def grafika_plansza(n: int):
-    if n == 3:
+def grafika_plansza(b_size: int):
+    if b_size == 3:
         return pygame.image.load("./Plansze/Plansza_3x3_v1.png")
-    elif n == 4:
+    elif b_size == 4:
         return pygame.image.load("./Plansze/plansza_4x4.png")
-    elif n == 5:
+    elif b_size == 5:
         return pygame.image.load("./Plansze/plansza_5x5.PNG")
 
 
@@ -45,7 +45,7 @@ clock = pygame.time.Clock()
 
 
 # ekran główny
-def game_intro(n: None = None):
+def game_intro():
     intro = True
 
     while intro:
@@ -56,18 +56,19 @@ def game_intro(n: None = None):
         button1 = buttons.Button(290, 220, 210, 60, '3x3', game_menu, 3)
         button2 = buttons.Button(290, 300, 210, 60, '4x4', game_menu, 4)
         button3 = buttons.Button(290, 380, 210, 60, '5x5', game_menu, 5)
+        button4 = buttons.Button(290, 460, 210, 60, 'Exit', sys.exit)
 
-        intro = not (button1.button_down or button2.button_down or button3.button_down)
+        intro = not (button1.button_down or button2.button_down or button3.button_down or button4.button_down)
 
         pygame.display.update()
         clock.tick(100)
     pass
 
 
-def game_menu(n: int = None):
+def game_menu(size: int = None):
 
-    def start(m: int = None):
-        plansza(n, m)
+    def start(g_type: int = None):
+        plansza(size, g_type)
 
     run = True
 
@@ -90,14 +91,14 @@ def game_menu(n: int = None):
     pass
 
 
-def plansza(n: int = None, m: int = None):
+def plansza(size: int = None, g_type: int = None):
     game = True
 
-    board = fgraph.Board(n)
+    board = fgraph.Board(size)
 
     while game:
 
-        screen.blit(pygame.transform.scale(grafika_plansza(n), (800, 600)), (0, 0))
+        screen.blit(pygame.transform.scale(grafika_plansza(size), (800, 600)), (0, 0))
 
         for figure in board.get_elements:
             if figure[0] == 1:
@@ -113,21 +114,21 @@ def plansza(n: int = None, m: int = None):
                 pos = pygame.mouse.get_pos()
 
                 if event.button == 1:
-                    board.add_figure(pos[0], pos[1])                                    # here adding a new cross/circle
+                    board.add_figure(pos[0], pos[1])                                # here adding a new cross/circle
 
-                    if m == 2:                                                          # here optional computer move mordo
+                    if g_type == 2:                                                 # here optional computer move mordo
                         board.computers_move()
 
-                    winner = game_end.ChekingBoard(board=board.matrix, size=n).check()  # here checking for a win
+                    winner = game_end.ChekingBoard(board=board.matrix, size=size).check()  # here checking for a win
 
-                    if winner == 1:                                                     # TODO: rewrite it's ugly
-                        print("wygrały kółka")                                          # TODO: exception handling mordo
+                    if winner == 1:                                                 # TODO: rewrite it's ugly
+                        print("wygrały kółka")                                      # TODO: exception handling mordo
                         break
                     elif winner == -1:
                         print("wygrały krzyżyki")
                         break
 
-            if event.type == pygame.QUIT:                                               # here checking for a quit event
+            if event.type == pygame.QUIT:                                           # here checking for a quit event
                 sys.exit()
             if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                 sys.exit()
@@ -135,11 +136,11 @@ def plansza(n: int = None, m: int = None):
         pygame.display.update()
         clock.tick(100)
 
-    game_menu(n)
+    game_menu(size)
     pass
 
 
 # mian loop
 def main_loop():
-    game_intro(None)
+    game_intro()
     sys.exit()
