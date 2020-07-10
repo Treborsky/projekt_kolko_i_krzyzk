857e9da32f14f741b529ef73d77e1ea3af2169ea
Robert Koziarski <rob.koziarski2k@gmail.com>
2020-07-10 13:46:33 +0200

    można grać z komputerem

--- source/comp.py
+++ source/comp.py
@@ -15,9 +15,9 @@ def computer_move(board, mark_bool: bool)->Tuple[int,int]:
     else:
         mark=-1
 
-    def ruch(poz_x, poz_y, mark, matrix):
+    def ruch(poz_x, poz_y, matrix):
         if matrix[poz_x % 3][poz_y % 3] == 0:
-            return((poz_x%3, poz_y%3))
+            return poz_x%3, poz_y%3
 
     def order(matrix):
         lst = []
@@ -46,47 +46,47 @@ def computer_move(board, mark_bool: bool)->Tuple[int,int]:
                 s += 1
         return s
 
-    def fajen(i, A, mark):
+    def fajen(i, A):
         B = order(A)
         if i >= 0 and i < 3:
             for j in range(3):
                 if B[i][j] == 0:
-                    return ruch(j, i, mark, A)
+                    return ruch(j, i, A)
         elif (i >= 3 and i < 6):
             for j in range(3):
                 if B[j][i % 3] == 0:
-                    return ruch(j, i, mark, A)
+                    return ruch(j, i, A)
         elif i == 6:
             for j in range(3):
                 if B[i][j] == 0:
-                    return ruch(j, j, mark, A)
+                    return ruch(j, j, A)
         elif i == 7:
             for j in range(3):
                 if B[i][j] == 0:
-                    return ruch(j, 2 - j, mark, A)
+                    return ruch(j, 2 - j, A)
 
     def rd():
         return random.choice([0, 1, 2])
 
-    def comp_moves(A, mark):
+    def comp_moves(A):
         Ord = order(A)
         for i in range(len(Ord)):
             if how_much(mark, Ord[i]) == 2:
-                return fajen(i, A, mark)
+                return fajen(i, A)
 
         for i in range(len(Ord)):
             if how_much(-mark, Ord[i]) == 2:
-                return fajen(i, A, mark)
+                return fajen(i, A)
 
         for i in Ord:
             if how_much(mark, Ord) == 1 and how_much(-mark, Ord) == 0:
-                return fajen(i, A, mark)
+                return fajen(i, A)
 
         L = 1
         while L != 0:
             x = rd()
             y = rd()
             L = A[x][y]
-        ruch(x, y, mark, A)
-        return
-    return comp_moves(board, mark)
+        return ruch(x, y, A)
+
+    return comp_moves(board)
